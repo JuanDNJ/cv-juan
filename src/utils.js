837258fe7194e21d7c95d29610bcd5b335a43084
JuanDNJ <juandevnjv@gmail.com>
2024-05-17 09:01:45 +0200

    "new file:   src/utils.js"

---
+++ src/utils.js
@@ -0,0 +1,16 @@
+const LANGUAGES = [
+  {
+    name: "ca",
+    icon: "images/cat.png",
+  },
+  {
+    name: "es",
+    icon: "images/es.png",
+  },
+  {
+    name: "en",
+    icon: "images/en.png",
+  },
+];
+
+export { LANGUAGES };
